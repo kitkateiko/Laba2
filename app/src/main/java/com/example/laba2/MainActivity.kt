bc7f772ae4ade450efdd66658f9e991fd1f1d0ee
kitkateiko <tyrkov2003@mail.ru>
2024-09-20 17:51:52 +0700

    Interface

--- app/src/main/java/com/example/laba2/MainActivity.kt
+++ app/src/main/java/com/example/laba2/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+import android.view.View
+import android.widget.EditText
+import java.math.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun formula(view: View)
+    {
+        val x = findViewById<EditText>(R.id.editTextNumber).text.toString().toDouble()
+        var y = 1.0
+        for(n in 1..7)
+        {
+            y *= (x-Math.pow(2.0,n.toDouble()))/(x-(Math.pow(2.0,n.toDouble())-1))
+        }
+        Toast.makeText(this,y.toString(),Toast.LENGTH_SHORT).show()
     }
 }
